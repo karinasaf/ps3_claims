1079f0a680d5a6aa110b115954056411588c125a
karinasaf <156846260+karinasaf@users.noreply.github.com>
2025-11-20 19:23:01 +0000

    added the sample split function

--- ps3/data/_sample_split.py
+++ ps3/data/_sample_split.py
@@ -1,5 +1,5 @@
 import hashlib
-
+import pandas as pd
 import numpy as np
 
 # TODO: Write a function which creates a sample split based in some id_column and training_frac.
@@ -21,5 +21,17 @@ def create_sample_split(df, id_column, training_frac=0.8):
     pd.DataFrame
         Training data with sample column containing train/test split based on IDs.
     """
-
+    columns = id_column if isinstance(id_column, list) else [id_column]
+        
+    def int_from_row(r):
+        vals = [str(v) if pd.notna(v) else "__MISSING__" for v in r]
+        key_str = "|".join(vals)
+        b = key_str.encode("utf-8")
+        h = hashlib.md5(b).digest()
+        return int.from_bytes(h, byteorder='big')
+    
+    threshold = int(training_frac * 100)
+    ids = df[columns].apply(int_from_row, axis=1)
+    buckets = ids % 100
+    df['sample'] = np.where(buckets < threshold, 'train', 'test')
     return df
